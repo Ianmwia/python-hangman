578d85689bc66354f0771d05ed01b2346d0cd8fd
Mwia <iansnr10@gmail.com>
2025-12-02 19:33:27 +0300

    creating a dictionary of words

--- words.py
+++ words.py
@@ -1,1 +1,13 @@
-words = ['apple', 'mango', 'abercrombie']
+words = {
+    'computer parts': ['mouse', 'keyboard', 'processor', 'motherboard'],
+    'planets': ['venus', 'earth', 'mercury', 'uranus', 'neptune'],
+    'fruits': ['papaya', 'banana', 'pineapple', 'tomato'],
+    'superhero': ['spiderman', 'superman', 'batman', 'flash'],
+    'mythical creatures': ['dragon', 'unicorn', 'phoenix', 'mermaid'],
+    'asian countries': ['Turkmenistan', 'china', 'vietnam', 'philippines'],
+    'famous scientists': ['einstein', 'tesla', 'newton', 'galileo'],
+    'big five': ['lion', 'elephant', 'rhino', 'leopard', 'buffalo'],
+    'music genres': ['rock', 'pop', 'rap', 'classical', 'reggae'],
+    'sports': ['football', 'basketball', 'rugby', 'tennis'],
+    'video games' : ['pacman', 'fortnite', 'pong', 'halo']
+}
